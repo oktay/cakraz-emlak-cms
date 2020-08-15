c9ffd68dabcd1579ebd39495269a984aec204e1a
oktay <oktaycolakoglu@gmail.com>
2020-08-15 14:00:18 +0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c1dd61f3cb8dfa7bf083d236139308c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '804be05d-0d97-4051-8355-a2c8d047d59b'
+};
