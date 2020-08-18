e995354b31025d04a3d3a60297c135fe8ee28553
oktay <oktaycolakoglu@gmail.com>
2020-08-18 20:13:29 +0300

    cloudinary provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,10 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "ftp-v2",
+    provider: 'cloudinary',
     providerOptions: {
-      host: env("FTP_HOST"),
-      port: env("FTP_PORT"),
-      user: env("FTP_USER"),
-      password: env("FTP_PASSWORD"),
-      basePath: env("FTP_BASEPATH"),
-      baseUrl: env("FTP_BASEURL"),
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
     },
   },
 });
